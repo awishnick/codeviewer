b81ed114154e8829233fbbc5e270525e4980e017
Aaron Wishnick <awishnick@server.fake>
2013-02-19 00:18:14 -0500

    Display diagnostics for files that have them.

--- restserver.py
+++ restserver.py
@@ -30,7 +30,7 @@ class CodeViewer:
 
         self.usrs = find_all_usrs(self.tus, self.sources)
 
-    def filename_from_id(self, idx):
+    def id_to_filename(self, idx):
         return self.sources[idx]
 
     def filename_to_id(self, idx):
@@ -40,6 +40,82 @@ class CodeViewer:
         """Return the contents of the given source file."""
         with open(os.path.join(self.input_dir, src), 'r') as f:
             return f.read()
+
+    def get_tu_from_id(self, idx):
+        return self.tus[self.id_to_filename(idx)]
+
+    def id_to_abs_filename(self, idx):
+        return self.abs_sources[idx]
+
+    def get_all_diagnostics(self):
+        """Collect all diagnostics across translation units.
+
+        The return value is a dictionary mapping from file ID to a set of
+        Diagnostic objects. Diagnostics occurring in files that are not indexed
+        are ignored. Duplicate diagnostics are ignored.
+        """
+
+        diags = {}
+        for tu in self.tus.itervalues():
+            for diag in tu.diagnostics:
+                if diag.location.file is None:
+                    continue
+
+                filename = diag.location.file.name
+                if filename not in self.abs_sources:
+                    continue
+
+                if filename not in diags:
+                    diags[filename] = set()
+                diags[filename].add(diag)
+
+        return diags
+
+
+class ClangEncoder(json.JSONEncoder):
+    """JSON encoder for clang cindex objects."""
+    def __init__(self, rel_dir):
+        json.JSONEncoder.__init__(self)
+        self.rel_dir = rel_dir
+
+    def default(self, obj):
+        if isinstance(obj, cindex.File):
+            return os.path.relpath(obj.name, self.rel_dir)
+
+        if isinstance(obj, cindex.SourceRange):
+            return {
+                'start': self.default(obj.start),
+                'end': self.default(obj.end),
+            }
+
+        if isinstance(obj, cindex.SourceLocation):
+            return {
+                'file': self.default(obj.file),
+                'line': obj.line,
+                'column': obj.column,
+            }
+
+        if isinstance(obj, cindex.Diagnostic):
+            severity_strs = {
+                cindex.Diagnostic.Ignored: 'Ignored',
+                cindex.Diagnostic.Note: 'Note',
+                cindex.Diagnostic.Warning: 'Warning',
+                cindex.Diagnostic.Error: 'Error',
+                cindex.Diagnostic.Fatal: 'Fatal',
+            }
+            js = {
+                'severity': severity_strs[obj.severity],
+                'location': self.default(obj.location),
+                'spelling': obj.spelling
+            }
+
+            ranges = [self.default(r) for r in obj.ranges]
+            if ranges:
+                js['ranges'] = ranges
+
+            return js
+
+        return json.JSONEncoder.default(self, obj)
 
 
 @app.route('/api/sources')
@@ -55,7 +131,7 @@ def api_sources():
 def api_show_source(idx):
     idx = int(idx)
     try:
-        filename = codeviewer.filename_from_id(idx)
+        filename = codeviewer.id_to_filename(idx)
     except:
         raise NotFound()
 
@@ -64,7 +140,16 @@ def api_show_source(idx):
         'id': idx,
         'contents': codeviewer.read_source(filename),
     }
-    js = json.dumps(obj)
+
+    abs_filename = codeviewer.id_to_abs_filename(idx)
+    try:
+        diags = list(codeviewer.get_all_diagnostics()[abs_filename])
+    except KeyError:
+        diags = []
+    if diags:
+        obj['diagnostics'] = diags
+
+    js = ClangEncoder(codeviewer.input_dir).encode(obj)
     resp = Response(js, mimetype='application/json')
     return resp
 
@@ -79,31 +164,13 @@ def api_usrs():
 
 @app.route('/api/usrs/usr/<usr>')
 def api_show_usr(usr):
-    class ClangEncoder(json.JSONEncoder):
-        rel_dir = codeviewer.input_dir
-
-        def default(self, obj):
-            if isinstance(obj, cindex.SourceRange):
-                return {
-                    'start': self.default(obj.start),
-                    'end': self.default(obj.end),
-                }
-            if isinstance(obj, cindex.SourceLocation):
-                return {
-                    'filename': os.path.relpath(obj.file.name,
-                                                ClangEncoder.rel_dir),
-                    'line': obj.line,
-                    'column': obj.column,
-                }
-
-            return json.JSONEncoder.default(self, obj)
     node = codeviewer.usrs[usr]
     nodeobj = {
         'usr': usr,
         'displayname': node.displayname,
         'extent': node.extent
     }
-    js = json.dumps({'node': nodeobj}, cls=ClangEncoder)
+    js = ClangEncoder(codeviewer.input_dir).encode({'node': nodeobj})
     resp = Response(js, mimetype='application/json')
     return resp
 
